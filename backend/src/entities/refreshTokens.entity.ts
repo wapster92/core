8f2fca33fc075597fbcbabd238dc36d7beae2f74
WapSter <tursunbaev@indins.ru>
2021-12-21 09:54:16 +0300

    testing crud

---
+++ backend/src/entities/refreshTokens.entity.ts
@@ -0,0 +1,14 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Users } from './users.entity';
+
+@Entity()
+export class RefreshTokens {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => Users, (user) => user.refreshTokens)
+  user: Users;
+
+  @Column()
+  token: string;
+}
